86a78b662f50c0f0621fdd0db74bbe3732df10ca
noghri <noghri@gmail.com>
2013-10-17 14:38:08 -0400

    fixed weather

--- moocow.pl
+++ moocow.pl
@@ -237,7 +237,7 @@ sub weather {
 
     my $wun = new WWW::Wunderground::API(location => $zip, api_key => $apikey, auto_api => 1,  cache=>Cache::FileCache->new({ namespace=>'moocow_wundercache', default_expires_in=>2400 }));
 
-    if($wun->response->error)
+    if($wun->response->error->description)
     {
         $irc->yield(privmsg => $chan => "No results: " . $wun->response->error->description);
         return;
