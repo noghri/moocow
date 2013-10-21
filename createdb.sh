1060045514d233e8fc4e830c8bafc3103488dd95
Aaron Sethman <androsyn@ratbox.org>
2013-10-21 14:40:30 -0400

    New schema..breaking stuff :D

--- createdb.sh
+++ createdb.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+if [ -f $1 ]; then
+	echo "File $1 exists, do you which to overwrite? Y/N"
+	read YN 
+	if [ x$YN != 'xY' ]; then
+		echo "Got $YN aborting..."
+		exit 1
+	fi
+	rm $1
+fi
 
 
-sqlite3 moocow.db <<EOF
+sqlite3 $1 <<EOF
 	CREATE TABLE quotes (quote, timestamp, usermask, channel, quoteid integer primary key autoincrement);
-	CREATE TABLE users (username, host, access, wzdefault , channel, userid integer primary key autoincrement);
+	CREATE TABLE users (username unique, access, wzdefault , userid integer primary key autoincrement);
+	CREATE TABLE usermask (hostmask, userid integer, foreign key(userid) REFERENCES users(userid));
+	CREATE TABLE channel (channame unique, ownerid, chanid integer primary key autoincrement, foreign key(ownerid) REFERENCES users(userid));
+	CREATE TABLE chanuser (chaccess, userid);
 EOF
