178e170d768bda3082496e6032fff79d251f62e2
Aaron Sethman <androsyn@ratbox.org>
2013-11-01 16:40:19 -0400

    add a new column

--- createdb.sh
+++ createdb.sh
@@ -21,7 +21,7 @@ sqlite3 $FILENAME <<EOF
         CREATE TABLE rssfeeds (nick, title, rssurl, titleid integer primary key autoincrement);
 	CREATE TABLE quotes (quote, timestamp, usermask, channel, quoteid integer primary key autoincrement);
 	CREATE TABLE users (username unique, access, wzdefault , userid integer primary key autoincrement);
-	CREATE TABLE usermask (hostmask, userid integer not null, foreign key(userid) REFERENCES users(userid) ON DELETE CASCADE);
+	CREATE TABLE usermask (hostmask, userid integer not null, maskid integer primary key autoincrement, foreign key(userid) REFERENCES users(userid) ON DELETE CASCADE);
 	CREATE TABLE channel (channame unique, chankey, ownerid not null, chanid integer primary key autoincrement, foreign key(ownerid) REFERENCES users(userid));
 	CREATE TABLE chanuser (chaccess, userid not null, chanid integer not null, chuserid integer primary key autoincrement, foreign key(chanid) REFERENCES channel(chanid) ON DELETE CASCADE, foreign key(userid) REFERENCES users(userid));
 	
